7cab09b784b23b1d41ffa84d689d3ff3f09fb2f5
JM-Dev <jtogni.developer@gmail.com>
2025-02-08 18:11:43 -0300

    blog

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from "astro:content";
+
+const posts = defineCollection({
+  schema: z.object({
+    id: z.number(),
+    title: z.string(),
+    description: z.string(),
+    date: z.string(),
+    author: z.string(),
+    img: z.string(),
+    categories: z.array(z.string()),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = { posts };
