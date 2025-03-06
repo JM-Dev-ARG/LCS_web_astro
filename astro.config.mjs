051757b6852ff663b07f8c24965475809efef271
JM-Dev <jtogni.developer@gmail.com>
2025-03-06 16:18:07 -0300

    cambiar el adaptador de Cloudflare a Netlify en la configuración de Astro y actualizar la dependencia en package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
+
+import netlify from '@astrojs/netlify';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,7 +18,5 @@ export default defineConfig({
   ), sitemap()],
 
   site: 'https://lcs-astro.netlify.app/',
-  adapter: cloudflare({
-    imageService: 'cloudflare'
-  }),
+  adapter: netlify(),
 });
