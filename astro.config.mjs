2abad1932eb181fa244f4b3830fa6ad04a29c9ef
JM-Dev <jtogni.developer@gmail.com>
2025-03-06 15:34:06 -0300

    agregar dependencia de @astrojs/cloudflare y ajustar importaciones en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import preact from '@astrojs/preact';
+
 
 import netlify from '@astrojs/netlify';
 
 import react from '@astrojs/react';
 
 import sitemap from '@astrojs/sitemap';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
