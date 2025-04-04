c8bcf4b1395b2b5c3b4ec3d31015df042aa208d3
JM-Dev <jtogni.developer@gmail.com>
2025-04-04 16:10:47 -0300

    refactor: eliminar llamada a postDataDDBB en downloadApi.ts para simplificar la lógica de respuesta

--- src/pages/api/downloadApi.ts
+++ src/pages/api/downloadApi.ts
@@ -1,18 +1,13 @@
 import type { APIRoute } from "astro";
 import postDataMailer from "../../lib/postDataMailer";
-import { postDataDDBB } from "../../lib/postDataDDBB";
 
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
 
   try {
     const mailerResponse = await postDataMailer(data);
-    const dbResponse = await postDataDDBB(data);
 
-    if (
-      (mailerResponse === 200 || mailerResponse === 201) &&
-      dbResponse.status === 200
-    ) {
+    if (mailerResponse === 200 || mailerResponse === 201) {
       return new Response(
         JSON.stringify({
           success: true,
@@ -26,7 +21,6 @@ export const POST: APIRoute = async ({ request }) => {
           success: false,
           message: "Hubo un problema con alguna de las operaciones.",
           mailerResponse,
-          dbResponse,
         }),
         { status: 500 }
       );
