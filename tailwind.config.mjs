8c3aed1bca8d5569342b66066b6ea07d503a55a5
JM-Dev <jtogni.developer@gmail.com>
2025-05-06 15:31:32 -0300

    Add font-face declarations for Poppins, Cormorant Garamond, and Darker Grotesque fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,17 @@ import typography from '@tailwindcss/typography'
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+
+
 	theme: {
 		extend: {
+
+			fontFamily: {
+				'serif': ['"Cormorant Garamond"', 'serif',],
+				'sans': ['"Poppins"', 'sans-serif',],
+				'display': ['"Darker Grotesque"', 'sans-serif'],
+
+			},
 			colors: {
 				lcsBlack: "#1d1e1c",
 				lcsGreen1: "#254542",
