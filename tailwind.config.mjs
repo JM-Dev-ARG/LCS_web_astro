eb89099f5289f1a6df89d43147626deba65cf32d
Juan Martinez <jtogni.developer@gmail.com>
2025-08-06 16:27:42 -0300

    refactor: mejorar la estructura y formato del archivo de configuración de Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,42 +1,36 @@
-import typography from '@tailwindcss/typography'
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 
-
-	theme: {
-		extend: {
-
-			fontFamily: {
-				'serif': ['"Cormorant Garamond"', 'serif',],
-				'sans': ['"Poppins"', 'sans-serif',],
-				'display': ['"Darker Grotesque"', 'sans-serif'],
-
-			},
-			colors: {
-				lcsBlack: "#1d1e1c",
-				lcsGreen1: "#254542",
-				lcsGreen2: "#083f37",
-				lcsGreen3: "#455e54",
-				lcsOrange1: "#fef0e9",
-				lcsOrange2: "#ea4d50",
-				lcsOrange3: "#ed6f5f",
-				lcsPink1: "#e69c99",
-				lcsPink2: "#fcded0",
-				lcsPink3: "#f2a9b1",
-
-			},
-			screens: {
-				'3xl': '1720px',
-
-			},
-			cursor: {
-				'scroll': 'url(/resources/scroll-icon.svg), pointer',
-			}
-		},
-
-	},
-	plugins: [typography],
-}
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Cormorant Garamond", "serif"],
+        sans: ["Poppins", "sans-serif"],
+        display: ["Darker Grotesque", "sans-serif"],
+      },
+      colors: {
+        lcsBlack: "#1d1e1c",
+        lcsGreen1: "#254542",
+        lcsGreen2: "#083f37",
+        lcsGreen3: "#455e54",
+        lcsOrange1: "#fef0e9",
+        lcsOrange2: "#ea4d50",
+        lcsOrange3: "#ed6f5f",
+        lcsPink1: "#e69c99",
+        lcsPink2: "#fcded0",
+        lcsPink3: "#f2a9b1",
+      },
+      screens: {
+        "3xl": "1720px",
+      },
+      cursor: {
+        scroll: "url(/resources/scroll-icon.svg), pointer",
+      },
+    },
+  },
+  plugins: [typography],
+};
